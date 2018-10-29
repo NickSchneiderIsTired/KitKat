c3985074894571f87844b875ae7208a131641319
Nick Schneider <nick-s2000@web.de>
2018-10-29 15:30:13 +0000

    Clean up

--- 01KitKat/a3/a3d.c
+++ 01KitKat/a3/a3d.c
@@ -3,7 +3,7 @@
 int main(int argc, char * argv[])
 {
 	int anzahl;
-	anzahl = printf("%s\n",argv[1])-1;
-	printf("%i",anzahl);
+	anzahl = printf("%s\n", argv[1]) - 1;
+	printf("%i", anzahl);
 	return 0;
 }
