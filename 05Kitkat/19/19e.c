37d79d21adfda47d4cc3751fa82afb3eae99a6ae
Steakie <nick-s2000@web.de>
2018-11-26 22:02:52 +0100

    Aufgabe 19e

--- 05Kitkat/19/19e.c
+++ 05Kitkat/19/19e.c
@@ -1,1 +1,14 @@
-TODO
+int algorithm(int values[], int s)
+{
+    int li = 0;
+    int re = sizeof(values) / sizeof(int) + 1;
+    while(li < re - 1) {
+        int m = (li + re) / 2;
+        if(s <= values[m]) {
+            re = m;
+        } else {
+            li = m;
+        }
+    }
+    return re;
+}
