f3c4e0c45309ac134c4437fced198fd432a54d3d
Steakie <nick-s2000@web.de>
2018-11-26 11:23:40 +0100

    Aufgabe 19a, c

--- 05Kitkat/19/19a.c
+++ 05Kitkat/19/19a.c
@@ -1,1 +1,19 @@
-TODO
+#include <stdio.h>
+
+void print_array(int w[], int n)
+{
+    int i;
+    for(i = 0; i < n; ++i) {
+        if(i == n - 1) {
+            printf("%i", w[i]);
+        } else {
+            printf("%i, ", w[i]);
+        }
+    }
+}
+
+int main(void)
+{
+    int values[] = {23, 2, 1, 5};
+    print_array(values, 3);
+}
--- 05Kitkat/19/19c.c
+++ 05Kitkat/19/19c.c
@@ -1,1 +1,16 @@
-TODO
+#include <stdio.h>
+
+void smult(int w[], int n, int x)
+{
+    int i;
+    for(i = 0; i < n; ++i) {
+        w[i] = w[i] * x;
+        printf("%i, ", w[i]);
+    }
+}
+
+int main(void)
+{
+    int values[] = {2, 3, 4, 5};
+    smult(values, 4, 2);
+}
