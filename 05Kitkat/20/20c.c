7b0410b2807758b768d74dbf3e4a88bfdd82cdb4
Steakie <nick-s2000@web.de>
2018-11-26 17:27:15 +0100

    Aufgabe 20

--- 05Kitkat/20/20c.c
+++ 05Kitkat/20/20c.c
@@ -1,12 +1,12 @@
 int ispalindrom(const char ct[])
 {
     int length = 0;
-    while(ct[length] != '\n') {
+    while(ct[length] != '\0') {
         ++length;
     }
     int i;
-    for(i = 0; i < length / 2; ++i) {
-        if(ct[i] != ct[length - 1 - i])) {
+    for( i = 0; i < length / 2; ++i) {
+        if(ct[i] != ct[length - 1 - i]) {
             return 0;
         }
     }
--- 05Kitkat/20/20d.c
+++ 05Kitkat/20/20d.c
@@ -1,1 +1,27 @@
-TODO
+#include <stdio.h>
+
+void strdel(char ct[], char c)
+{
+    int length;
+    while(ct[length] != '\0') {
+        ++length;
+    }
+    int positions[length + 1];
+    int i, j;
+    for(i = 0; i < length; ++i) {
+        if(ct[i] == c) {
+            for(j = i; j < length - 1; ++j) {
+                ct[j] = ct[j + i];
+            }
+            --length;
+        }
+    }
+}
+
+int main()
+{
+    char chars[] = "Hello";
+    strdel(chars, 'l');
+    printf("%s", chars);
+    return 0;
+}
