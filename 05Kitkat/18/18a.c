f7b4b3c6d9d0551010df91db26dbe60e02591cf4
Steakie <nick-s2000@web.de>
2018-11-26 10:33:16 +0100

    Aufgabe 18 (Aufgabe 18a anders, aber besser gelöst)

--- 05Kitkat/18/18a.c
+++ 05Kitkat/18/18a.c
@@ -1,1 +1,28 @@
-TODO
+#include <stdio.h>
+#include <ctype.h>
+
+void flush_buffer()
+{
+    while(getchar() != '\n') {}
+}
+
+int readDigit(char * c)
+{
+    if(scanf("%c", c) == 1 && isdigit(* c)) {
+        return 1;
+    } else {
+        flush_buffer();
+        return 0;
+    }
+}
+
+int main(void)
+{
+    char c;
+    printf("Bitte Ziffer eingeben");
+    while(!readDigit(&c)) {
+        printf("Eingabe nicht erfolgreich, bitte Ziffer eingeben\n");
+    }
+    printf("Eingabe erfolgreich, gelesene Ziffer: %c\n", c);
+    return 0;
+}
--- 05Kitkat/18/18b.c
+++ 05Kitkat/18/18b.c
@@ -1,1 +1,26 @@
-TODO
+#include <stdio.h>
+#include <ctype.h>
+
+void flush_buffer()
+{
+    while(getchar() != '\n') {}
+}
+
+int read_number(void)
+{
+    int i;
+    if(scanf("%i", &i) == 1 && i > 7 && i < 41) {
+        return i;
+    } else {
+        flush_buffer();
+        return -1;
+    }
+}
+
+int main(void)
+{
+    while(read_number() == -1) {
+        printf("Bitte ganze Zahl zwischen 8 und 40 eingeben");
+    }
+    printf("Eingabe gueltig");
+}
