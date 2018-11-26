8ce31da258a22c3df67ff663931b6776768d5201
Sidonitor <gregor.pfister@gmx.net>
2018-11-26 12:03:37 +0100

    Lösung aufg 17, 18, 19

--- 05Kitkat/17/17e.c
+++ 05Kitkat/17/17e.c
@@ -3,7 +3,7 @@ double sin(double x, int precision)
     double y = 1;
     int i = 1;
     for (i = 1; i < precision; i++){
-        y *= 1 - (x * x)/((i * i) * (M_PI * M_PI));;
+        y *= 1 - (x * x)/((i * i) * (M_PI * M_PI));
     }
     printf("%f\n", y);
     printf("%f\n", x);
