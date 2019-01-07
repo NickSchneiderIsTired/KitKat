763684a2721a4bc13ffcef33514506c4e35e74a2
Steakie <nick-s2000@web.de>
2019-01-07 12:26:24 +0100

    Clean up

--- 09KitKat/36/a36d.c
+++ 09KitKat/36/a36d.c
@@ -1,36 +1,4 @@
-#include <stdio.h>
-
-char *my_strpbrk(const char *str1, const char *str2, int m, int n)
-{
-    int i, j;
-    for(i = 0; i < m; ++i) {
-        for(j = 0; j < n; ++j) {
-            if(*(str1 + i) == *(str2 + j)) {
-                return str1 + i;
-            }
-        }
-    }
-    return NULL;
-}
-
-/*char *my_strpbrk2(const char *str1, const char *str2)
-{
-    int i = 0, j = 0;
-    while(*(str1 + i) != '\0') {
-        while(*(str2 + j) != '\0') {
-            if(*(str1 + i) == *(str2 + j)) {
-                return str1 + i;
-            }
-            j++;
-            printf("j: %i\n", j);
-        }
-        i++;
-        printf("i: %i\n", i);
-    }
-    return NULL;
-}*/
-
-char *my_strpbrk3(const char *str1, const char *str2)
+char *my_strpbrk(const char *str1, const char *str2)
 {
     int i, j;
     for(i = 0; *(str1 + i) != '\0'; ++i) {
@@ -41,11 +9,4 @@ char *my_strpbrk3(const char *str1, const char *str2)
         }
     }
     return NULL;
-}
-
-int main()
-{
-    char *p = my_strpbrk3("ello there", "wasght");
-    printf("%c", *p);
-    return 0;
 }
