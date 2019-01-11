8577feb716d7a6d56e7301d6d71a941ececdd8f3
Steakie <nick-s2000@web.de>
2019-01-11 18:22:20 +0100

    Aufgabe 37

--- 10KitKat/37/a37a.c
+++ 10KitKat/37/a37a.c
@@ -1,1 +1,30 @@
-TODO
+#include <stdio.h>
+#include <stdlib.h>
+
+int * cpy_array(int v[], int size)
+{
+    int *p;
+    int i;
+    p = malloc(size * sizeof(int));
+    if(p == NULL) {
+        free(p);
+        return NULL;
+    }
+    for(i = 0; i < size; i++) {
+        p[i] = v[i];
+    }
+    return p;
+}
+
+int main()
+{
+    int v[] = {7, 8, 9, 10};
+    int *newArray = cpy_array(v, 4);
+    if(newArray != NULL) {
+        printf("Victory Royale! Erfolgreich kopiert! Erster Wert: %i", newArray[0]);
+        return 0;
+    } else {
+        printf("Fehler beim kopieren");
+        return 1;
+    }
+}
--- 10KitKat/37/a37b.c
+++ 10KitKat/37/a37b.c
@@ -1,1 +1,26 @@
-TODO
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *safecat(char *s, const char *ct)
+{
+    if((realloc(s, (strlen(s) + strlen(ct)) * sizeof(char))) == NULL) {
+        return NULL;
+    }
+    strcat(s, ct);
+    return s;
+}
+
+int main()
+{
+    char * str1 = malloc(8 * sizeof(char));
+    strcpy(str1, "Pastewka");
+    char * str2 = "Stromberg";
+    if((str1 = safecat(str1, str2)) != NULL) {
+        printf("Victory Royale! %s", str1);
+        return 0;
+    } else {
+        printf("Fehler beim Anhängen");
+        return 1;
+    }
+}
