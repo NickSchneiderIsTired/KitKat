8c95dcc030f20b524e7d1f1d0c0d9ff1e64d6277
Steakie <nick-s2000@web.de>
2019-01-13 22:00:34 +0100

    Aufgabe 38e

---
+++ 10KitKat/38/a38e.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *orakel(void);
+
+int main(void) {
+    char *s;
+    s = orakel();
+    if (s == NULL) {
+        printf("main: Speicherfehler in orakel");
+        return 1;
+    }
+    printf("%s", s);
+    free(s);
+    return 0;
+}
+
+char *orakel(void) {
+    char *s, *t;
+    s = malloc(9 * sizeof(char));
+    if (s == NULL) {
+        free(s);
+        return NULL;
+    }
+    t = malloc(7 * sizeof(char));
+    if (t == NULL) {
+        free(t);
+        return NULL;
+    }
+    strcpy(s, "Six Feet");
+    strcpy(t, " Under");
+    strcat(s, t);
+    free(t);
+    return s;
+}
+
