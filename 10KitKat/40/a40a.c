41afa78a5ba1f5888c8bcc9f4448dcf66b646425
Steakie <nick-s2000@web.de>
2019-01-14 13:19:13 +0100

    Verbesserungen

--- 10KitKat/40/a40a.c
+++ 10KitKat/40/a40a.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
-
 
 int **matrix_cpy(int **m, int ze, int sp) /* sp wird nicht gebraucht wegen flacher Kopie*/
 {
-    int **newMatrix, i, k;
+    int **newMatrix, i;
     newMatrix = malloc(ze * sizeof(int*));
     if(!m) return NULL;
     for(i = 0; i < ze; ++i) {
