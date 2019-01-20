4592fde6fbe663a2e01da6c3aeac631aed0d4818
Steakie <nick-s2000@web.de>
2019-01-20 19:30:55 +0100

    Lösung

--- 11KitKat/41/a41d.c
+++ 11KitKat/41/a41d.c
@@ -1,1 +1,14 @@
-TODO
+#include <math.h>
+
+int func(unsigned int n)
+{
+    int t = 2;
+    while(t <= sqrt(n)) {
+        if(n % t != 0) {
+            ++t;
+        } else {
+            return 0;
+        }
+    }
+    return 1;
+}
--- 11KitKat/41/a41e.c
+++ 11KitKat/41/a41e.c
@@ -1,1 +1,12 @@
-TODO
+int func(char w[], char v[], int n, int m)
+{
+    int i = 1;
+    while(i <= m) {
+        if(w[i] != v[i]) {
+            return 0;
+        } else {
+            ++i;
+        }
+    }
+    return 1;
+}
