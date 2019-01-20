82404317b08c506578ad343286cccf01b3a9c957
Steakie <nick-s2000@web.de>
2019-01-20 19:31:41 +0100

    Lösung 2

--- 11KitKat/41/a41e.c
+++ 11KitKat/41/a41e.c
@@ -2,7 +2,7 @@ int func(char w[], char v[], int n, int m)
 {
     int i = 1;
     while(i <= m) {
-        if(w[i] != v[i]) {
+        if(w[i - 1] != v[i - 1]) {
             return 0;
         } else {
             ++i;
