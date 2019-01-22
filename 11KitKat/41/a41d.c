7d308e8bee0e31819c1498ad206b54313a9b2e5e
Sidonitor <gregor.pfister@gmx.net>
2019-01-22 12:19:24 +0100

    Lösung aufg 41

--- 11KitKat/41/a41d.c
+++ 11KitKat/41/a41d.c
@@ -1,1 +1,15 @@
-TODO
+#include <math.h>
+
+int function(int n)
+{
+    int t = 2;
+    while (t <= sqrt(n)){
+        if (n % t != 0){
+            ++t;
+        } else {
+            return 0;
+        }
+    }
+    return 1;
+}
+
--- 11KitKat/41/a41e.c
+++ 11KitKat/41/a41e.c
@@ -1,1 +1,10 @@
-TODO
+int isPrefix(char w[], char v, int n, int m)
+{
+    int i;
+    for (i = 1; i <= m; ++i){
+        if (w[i] != v [i])		{
+            return 0;
+        }
+    }
+    return 1;
+}
