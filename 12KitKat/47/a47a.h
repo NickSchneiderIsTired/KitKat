7aff5199e4cd97d27ee413b0cd5a079188fc7e1d
Steakie <nick-s2000@web.de>
2019-01-24 23:10:12 +0100

    47a

--- 12KitKat/47/a47a.h
+++ 12KitKat/47/a47a.h
@@ -1,10 +1,24 @@
 #ifdef RECHTECK_INCLUDED
 #define RECHTECK_INCLUDED
 
-typedef struct Rechteck {
-    unsigned double length;
-    unsigned double width;
+#define MIN_WIDTH 100
+#define MIN_LENGTH 100
 
-};
+typedef struct _rechteck {
+    double width;
+    double length;
+
+    double getWidth(Rechteck *r);
+    double getLength(Rechteck *r);
+
+    double getWidth(Rechteck *r, double x);
+    double setLength(Rechteck *r, double x);
+
+    double checkWidth(double x);
+    double checkLength(double x);
+
+    int new_rechteck(Rechteck r, double width, double length);
+
+} Rechteck;
 
 #endif
