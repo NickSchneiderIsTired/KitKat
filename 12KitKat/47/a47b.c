ec9e588d9bcb5a16b717742d6247ac7f356b9d79
Steakie <nick-s2000@web.de>
2019-01-24 23:20:40 +0100

    47b

--- 12KitKat/47/a47b.c
+++ 12KitKat/47/a47b.c
@@ -1,1 +1,18 @@
-TODO
+#include <stdio.h>
+
+int main(void)
+{
+    ARTIKEL a;
+    if((artikel_new(&a, "Bleistift", 0.0, 0.75)) == 0) {
+        printf("Fehler beim erstellen des Artikels");
+        return 1;
+    }
+    if((artikel_set_rabatt(&a, 10.0)) == 0) {
+        printf("Fehler beim setzen des Rabatts");
+        return 1;
+    } else {
+        printf("Setzen des Rabatts erfolgreich");
+    }
+    artikel_print(&a);
+    return 0;
+}
